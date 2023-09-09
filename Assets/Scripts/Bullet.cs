034c49fafb10994567c4c3fbd751338517b31b20
WS9191 <143865831+WS9191@users.noreply.github.com>
2023-09-09 22:14:33 +0800

    Update Bullet.cs
    bullet can move
    bullet is moving upwards
    bullet hit enemy will destroy
    
    the homing missile haven't done

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -14,16 +14,27 @@ Editable in editor
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject bullet;
+    public float TimeToDestray=10;
+    public float Speed = 10;
+    public int Damage;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, TimeToDestray);
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.AddForce(transform.up * Speed*Time.deltaTime);
+    }
+    // Update is called once per frame
+   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
